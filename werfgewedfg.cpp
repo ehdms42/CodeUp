e1066baac6963cfc186fa1a6e46ec14709ea3aa6
도은 <163129697+ehdms42@users.noreply.github.com>
2024-06-02 22:34:01 +0900

    Add files via upload

---
+++ werfgewedfg.cpp
@@ -0,0 +1,35 @@
+//#include <stdio.h>
+//int main()
+//{
+//	int a,b;
+//	scanf("%d %d", &a, &b);
+//	
+//	printf("%d", a%b);
+//	
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//int main(){
+//	int a;
+//	
+//	scanf("%d", &a);
+//	
+//	if(a==0) printf("1");
+//	else printf("0");
+//	
+//	return 0;
+//}
+
+
+#include <stdio.h>
+int main()
+{
+	double a;
+	scanf("%lf", &a);
+	a++;
+	printf("%.0lf", a);
+	
+	return 0;
+}
